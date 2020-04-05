8c5dee6a6f1b15089a8897c91352ecb5a8a22786
Guillermo Alfonso Varela Chouciño <guillevch@gmail.com>
2020-04-05 19:27:54 +0200

    feat: Tested project and added script to run a dockerized postgres instance

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :fraction_server, FractionServer.Repo,
   username: "postgres",
   password: "postgres",
-  database: "fraction_server_dev",
+  database: "fraction",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
